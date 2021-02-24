57d90b2db36b67ff239e0bed9f7d5dca948ab81b
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-02-23 19:12:37 -0600

    search results route

---
+++ app/api/browse_results_routes.py
@@ -0,0 +1,14 @@
+# from flask import Blueprint
+# from flask_login import current_user
+# from app.models import Pet
+
+# browse_results_routes = Blueprint('results', __name__)
+
+
+# @browse_results_routes('/results')
+# def browse_results(browse):
+#     results = []
+  
+
+#     if (current_user):
+       
