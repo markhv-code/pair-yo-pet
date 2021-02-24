ff0c2df17d4777d2a846463f3346c83bd82792c7
Mark Valdez <mhvaldez209@gmail.com>
2021-02-24 07:53:43 -1000

    demo login button

--- react-app/src/components/auth/LoginFormModal/LoginForm.js
+++ react-app/src/components/auth/LoginFormModal/LoginForm.js
@@ -19,6 +19,23 @@ function LoginForm() {
     }
   };
 
+  const demoLogin = async (e) => {
+    e.preventDefault();
+    setEmail("demo@aa.io");
+    setPassword("password");
+    let user;
+    setTimeout(() => {
+      user = login(email, password)
+      
+    }, 500)
+    dispatch(setUser(user));
+    // if(!user.errors){
+    //   dispatch(setUser(user));
+    // } else {
+    //   setErrors(user.errors);
+    // }
+  }
+
   const updateEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -28,34 +45,39 @@ function LoginForm() {
   };
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-      </div>
-      <button type='submit'>Login</button>
-    </form>
+    <>
+      <form onSubmit={onLogin}>
+        <div>
+          {errors.map((error) => (
+            <div>{error}</div>
+          ))}
+        </div>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+      <form onSubmit={demoLogin}>
+        <button type='submit'>Demo Login</button>
+      </form>
+  </>
   );
 }
 
