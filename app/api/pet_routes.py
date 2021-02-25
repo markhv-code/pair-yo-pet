86bce37909462b4d116bf7c9c628056cbacd4ba5
Zachary <duvallzt@gmail.com>
2021-02-25 03:52:24 -0700

    Add default dog url if no image is uploaded

--- app/api/pet_routes.py
+++ app/api/pet_routes.py
@@ -31,22 +31,25 @@ def create_pet():
     form = CreatePetForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
 
-    image = request.files["image"]
-    image.filename = secure_filename(image.filename)
-
     image_error = []
-    print("--------------image type----------------", image.mimetype)
-    pattern = re.compile(".*(apng|avif|jpe?g|png|svg|webp)$", re.IGNORECASE)
-    is_image = bool(pattern.match(image.mimetype))
+    image = request.files.get("image", None)
 
-    if not is_image:
-        image_error.append(
-            "Upload must be an image (apng, avif, jpeg/jpg, png, svg, webp)."
-        )
+    if image != None:
+        image.filename = secure_filename(image.filename)
+        pattern = re.compile(".*(apng|avif|jpe?g|png|svg|webp)$", re.IGNORECASE)
+        is_image = bool(pattern.match(image.mimetype))
+        if not is_image:
+            image_error.append(
+                "Upload must be an image (apng, avif, jpeg/jpg, png, svg, webp)."
+            )
 
     if form.validate_on_submit() and not image_error:
 
-        output_link = upload_file_to_s3(image)
+        output_link = (
+            upload_file_to_s3(image)
+            if image
+            else "https://pair-yo-pet-aws.s3-us-west-1.amazonaws.com/default-dog.png"
+        )
 
         new_pet = Pet(
             userId=form.data["userId"],
--- react-app/src/components/PetProfileForm/petProfileForm.js
+++ react-app/src/components/PetProfileForm/petProfileForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import { useParams } from "react-router-dom";
 import { createPet } from '../../store/pets';
 
 // // Material UI compenents, try later to get them to work
@@ -37,7 +36,6 @@ function PetProfileForm({ setShowModal }) {
       name: petName,
       petType,
       age,
-      // imageURL: imgUrl,
       image,
       energy,
       social,
