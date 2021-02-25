5f9eecdd738e7dd2a31893f3a5666a4691dabbb0
Zachary <duvallzt@gmail.com>
2021-02-25 14:21:54 -0700

    Add attributes to messages

--- app/models/message.py
+++ app/models/message.py
@@ -1,22 +1,29 @@
 from .db import db
 from datetime import datetime
 
+
 class Message(db.Model):
     __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
-    senderId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    receiverId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    senderId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    receiverId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     message = db.Column(db.String(200), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-    sender = db.relationship("User", foreign_keys=[senderId], back_populates="messages_sent")
-    receiver = db.relationship("User", foreign_keys=[receiverId], back_populates="messages_received")
+    sender = db.relationship(
+        "User", foreign_keys=[senderId], back_populates="messages_sent"
+    )
+    receiver = db.relationship(
+        "User", foreign_keys=[receiverId], back_populates="messages_received"
+    )
 
     def to_dict(self):
-        return{
-            "id":self.id,
+        return {
+            "id": self.id,
             "senderId": self.senderId,
             "receiverId": self.receiverId,
-            "message": self.message
+            "message": self.message,
+            "sender": self.sender.to_dict(),
+            "receiver": self.receiver.to_dict(),
         }
