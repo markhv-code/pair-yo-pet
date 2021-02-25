7f9d6aaef43bed9f6540c8a7d7db7175e632dbe2
Zachary <duvallzt@gmail.com>
2021-02-25 03:31:09 -0700

    Comment out unused import

--- react-app/src/components/NavBar/LoggedInDropdown.js
+++ react-app/src/components/NavBar/LoggedInDropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 import { logout } from '../../services/auth';
 import { removeUser } from '../../store/session';
