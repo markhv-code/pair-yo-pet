ef0ce86b680e50a4c459d6e957145def90b8fbfe
Mark Valdez <mhvaldez209@gmail.com>
2021-02-26 11:45:17 -1000

    add select drop down to stateAbrr in signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Email, ValidationError, Length, EqualTo
 from app.models import User
 
@@ -19,10 +19,17 @@ def email_exists(form, field):
         raise ValidationError("Email is already registered.")
 
 class SignUpForm(FlaskForm):
+    states = [
+        ("AK","AK"), ("AL","AL"), ("AR","AR"), ("AZ","AZ"), ("CA","CA"), ("CO","CO"), ("CT","CT"), ("DE","DE"), ("FL","FL"), ("GA","GA"), ("HI","HI"), ("IA","IA"), ("ID","ID"), ("IL","IL"), ("IN","IN"),
+        ("KS","KS"), ("KY","KY"), ("LA","LA"), ("MA","MA"), ("MD","MD"), ("ME","ME"), ("MI","MI"), ("MN","MN"), ("MO","MO"), ("MS","MS"), ("MT","MT"), ("NC","NC"), ("ND","ND"), ("NE","NE"), ("NH","NH"),
+        ("NJ","NJ"), ("NM","NM"), ("NV","NV"), ("NY","NY"), ("OH","OH"), ("OK","OK"), ("OR","OR"), ("PA","PA"), ("RI","RI"), ("SC","SC"), ("SD","SD"), ("TN","TN"), ("TX","TX"), ("UT","UT"), ("VA","VA"),
+        ("VT","VT"), ("WA","WA"), ("WI","WI"), ("WV","WV"), ("WY","WY")]
+
     username = StringField('username', validators=[DataRequired(message='Username must be between 1 and 30 characters.'),user_exists])
     email = StringField('email', validators=[DataRequired(
         message='Must input valid email!'), Email(message='Must input valid email!'), email_exists])
     city = StringField('city', validators=[DataRequired()])
-    stateAbbr = StringField('stateAbbr', validators=[DataRequired(), Length(
-        min=2, max=2, message='Must provide a two letter state abbreviation.')])
+    stateAbbr = SelectField('stateAbbr', choices=states)
+    # stateAbbr = StringField('stateAbbr', validators=[DataRequired(), Length(
+        # min=2, max=2, message='Must provide a two letter state abbreviation.')])
     password = PasswordField('password', validators=[DataRequired()])
