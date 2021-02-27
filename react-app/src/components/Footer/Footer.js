efaa4a5609edca8f087ccd50d3fe2d75c9837be8
Mark Valdez <mhvaldez209@gmail.com>
2021-02-26 22:24:24 -1000

    include footer styling

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,12 +1,28 @@
 import { useWindowWidth } from '../../services/windowWidth';
 
 
-const Footer = () {
+const Footer = () => {
     const width = useWindowWidth();
 
     return (
-        <div>
-        
+        <div className='footer-div'>
+            <div className='footer-div__person.footer-div__person-1'>
+                <a className='github-link' href='https://github.com/zduvall'>ZD</a>
+            </div>
+            <div className='footer-div__person.footer-div__person-2'>
+                <a className='github-link' href='https://github.com/CJNewcomer'>CN</a>
+            </div>
+            <div className="footer-div__github-link">
+                <a className='github-link github-repo' href='https://github.com/markhv-code/pair-yo-pet'>Github Repo Link</a>
+            </div>
+            <div className='footer-div__person.footer-div__person-3'>
+                <a className='github-link' href='https://github.com/MeeraMeera1'>AM</a>
+            </div>
+            <div className='footer-div__person.footer-div__person-4'>
+                <a className='github-link' href='https://github.com/markhv-code'>MV</a>
+            </div>
         </div>
     )
 }
+
+export default Footer;
