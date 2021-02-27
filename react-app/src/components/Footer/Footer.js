e7b48522aceb8fbdf8593ff3323da84723c54885
Mark Valdez <mhvaldez209@gmail.com>
2021-02-27 00:52:49 -1000

    finish adding github profile pics

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -5,20 +5,18 @@ const Footer = () => {
     return (
         <div className='footer-div'>
             <div className='footer-div__person zach'>
-                <a className='github-link' href='https://github.com/zduvall'>
-                <div></div>
-                </a>
+                <a className='github-link' href='https://github.com/zduvall'>ZD</a>
             </div>
-            <div className='footer-div__person'>
+            <div className='footer-div__person courtney'>
                 <a className='github-link' href='https://github.com/CJNewcomer'>CN</a>
             </div>
             <div className="footer-div__github-link">
                 <a className='github-link github-repo' href='https://github.com/markhv-code/pair-yo-pet'>Github Repo Link</a>
             </div>
-            <div className='footer-div__person'>
+            <div className='footer-div__person ameera'>
                 <a className='github-link' href='https://github.com/MeeraMeera1'>AM</a>
             </div>
-            <div className='footer-div__person'>
+            <div className='footer-div__person mark'>
                 <a className='github-link' href='https://github.com/markhv-code'>MV</a>
             </div>
         </div>
