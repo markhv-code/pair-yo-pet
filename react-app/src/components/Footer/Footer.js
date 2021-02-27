f0824bef1550c3e3273ee4077d38204c848c743e
Mark Valdez <mhvaldez209@gmail.com>
2021-02-26 23:04:04 -1000

    pretty good visual styling

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,24 +1,22 @@
-import { useWindowWidth } from '../../services/windowWidth';
-
+import './Footer.css'
 
 const Footer = () => {
-    const width = useWindowWidth();
 
     return (
         <div className='footer-div'>
-            <div className='footer-div__person.footer-div__person-1'>
+            <div className='footer-div__person'>
                 <a className='github-link' href='https://github.com/zduvall'>ZD</a>
             </div>
-            <div className='footer-div__person.footer-div__person-2'>
+            <div className='footer-div__person'>
                 <a className='github-link' href='https://github.com/CJNewcomer'>CN</a>
             </div>
             <div className="footer-div__github-link">
                 <a className='github-link github-repo' href='https://github.com/markhv-code/pair-yo-pet'>Github Repo Link</a>
             </div>
-            <div className='footer-div__person.footer-div__person-3'>
+            <div className='footer-div__person'>
                 <a className='github-link' href='https://github.com/MeeraMeera1'>AM</a>
             </div>
-            <div className='footer-div__person.footer-div__person-4'>
+            <div className='footer-div__person'>
                 <a className='github-link' href='https://github.com/markhv-code'>MV</a>
             </div>
         </div>
