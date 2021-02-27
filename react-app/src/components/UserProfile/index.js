f0bf9266014eeb4c97424a36a379364efc3a3229
MeeraMeera1 <gatesameera98@gmail.com>
2021-02-27 16:05:02 -0500

    trying to style

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -10,25 +10,46 @@ function User() {
   const user = useSelector((state) => state.users[userId]);
   const pets = useSelector((state) => Object.values(state.pets));
 
-  console.log(pets);
+  const mypets = pets.filter(pet => pet.userId === user.id)
+  console.log(mypets)
 
   // if (sessionUser.id !== user.id) return null;
   const { city, email, stateAbbr, username } = user
   return (
     <>
       <div className="profile-container">
-        <div>
-          <h1>Welcome {username}</h1>
-        </div>
-        <div>
-          <h2>My Info</h2>
-          <ul>
-            <li>{email}</li>
-            <li>{city},{stateAbbr}</li>
-          </ul>
+        <div className="user-info">
+          <div className="username">
+            <i className="fas fa-user-circle fa-lg" />
+            <h1>{username}</h1>
+          </div>
+          <div className="userInfo">
+            <div className="email">
+              <i className="fa fa-envelope" />
+              <h3>{email}</h3>
+            </div>
+            <div className="location">
+              <i className="fa fa-map-marker" />
+              <h3>
+                {city},{stateAbbr}
+              </h3>
+            </div>
+          </div>
         </div>
         <div>
           <h2>My Pets</h2>
+          {mypets.map((babies) => {
+            const { imageUrl, name, age } = babies;
+            return (
+              <div>
+                <img src={imageUrl} alt=""/>
+                <div>
+                  <h3>{name}</h3>
+                  <h3>{age}</h3>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
