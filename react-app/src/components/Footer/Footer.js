be47f026b3db1c733df35ed436a0507d6d11a3fb
Mark Valdez <mhvaldez209@gmail.com>
2021-02-26 22:12:02 -1000

    start footer component

---
+++ react-app/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import { useWindowWidth } from '../../services/windowWidth';
+
+
+const Footer = () {
+    const width = useWindowWidth();
+
+    return (
+        <div>
+        
+        </div>
+    )
+}
