ce442a5a86007569056ae22efa33267a2162433b
MeeraMeera1 <gatesameera98@gmail.com>
2021-02-27 23:22:42 -0500

    almost done

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -10,45 +10,42 @@ function User() {
   const pets = useSelector((state) => Object.values(state.pets));
 
   const mypets = pets.filter(pet => pet.userId === user.id)
-  console.log(mypets)
 
+  if(!user) return null;
   const { city, email, stateAbbr, username } = user
   return (
     <>
       <div className="profile-container">
         <div className="user-info">
           <div className="username">
-            <i className="fas fa-user-circle fa-lg" />
             <h1>{username}</h1>
           </div>
           <div>
             <div className="email">
-              <i className="fa fa-envelope" />
-              <h3>{email}</h3>
+              <i className="fa fa-envelope">{` ${email}`}</i>
             </div>
             <div className="location">
-              <i className="fa fa-map-marker" />
-              <h3>
-                {city},{stateAbbr}
-              </h3>
+              <i className="fa fa-map-marker">
+                {` ${city}, ${stateAbbr}`}
+              </i>
             </div>
           </div>
         </div>
         <div className="pets">
           <h2>My Pets</h2>
-        <div className="side__container">
-          {mypets.map((babies) => {
-            const { imageURL, name, age } = babies;
-            return (
-              <div className="right__container">
-                <img src={imageURL} alt="" />
-                <div>
-                  <h3>{name}</h3>
-                  <h3>{age}</h3>
+          <div className="side__container">
+            {mypets.map((babies) => {
+              const { imageURL, name, age } = babies;
+              return (
+                <div className="right__container">
+                  <img src={imageURL} alt="" />
+                  <div>
+                    <h3>{name}</h3>
+                    <h3>{age}</h3>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
           </div>
         </div>
       </div>
