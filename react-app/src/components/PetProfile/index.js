f41bb34896202da12e9d7a45d62aca8d12d8c2c0
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-29 15:41:23 -0500

    added hover effect on pet owner link

--- react-app/src/components/PetProfile/index.js
+++ react-app/src/components/PetProfile/index.js
@@ -63,7 +63,8 @@ const PetProfile = () => {
                         <h3>Age: {age}</h3>
                         <h3>{pet.owner.city}, {pet.owner.stateAbbr}</h3>
                         <h3 className='profile__owner-link'>
-                            <NavLink to={`/users/${pet.userId}`}>Owner: {pet.owner.username}</NavLink> </h3>
+                            <NavLink to={`/users/${pet.userId}`}>Owner: {pet.owner.username}</NavLink> 
+                        </h3>
                         <h4 className='profile__description'>{description}</h4>
                     </div>
                     <div className='personality__scales'>
