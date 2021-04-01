9dce3a7d802790fcfacc073e86a3a3eb74886c80
Zachary <duvallzt@gmail.com>
2021-04-01 11:15:50 -0700

    Remove unneededuseMemo

--- react-app/src/components/BrowsePets/index.js
+++ react-app/src/components/BrowsePets/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import { useHistory } from 'react-router-dom';
 import {getPets} from '../../store/pets'
